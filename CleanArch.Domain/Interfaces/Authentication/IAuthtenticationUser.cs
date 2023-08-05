c7c2740497a3e9c515d16728ac2d177ed3d7f0f5
daniel-kenobi <daniel.companyservicostecnicos@gmail.com>
2023-08-05 16:41:29 -0300

    More updates

---
+++ CleanArch.Domain/Interfaces/Authentication/IAuthtenticationUser.cs
@@ -0,0 +1,8 @@
+﻿namespace CleanArch.Domain.Interfaces.Authentication
+{
+    public interface IAuthtenticationUser
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
